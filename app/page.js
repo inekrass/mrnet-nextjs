374fb35d9044a29b857c3e0c979c1f99a4fbe5ed
inekrass <bogdan.nekpasrv@mail.ru>
2025-12-09 17:51:26 +0300

    fix

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Technology from "./components/Technology";
-import Monitoring from "./components/Monitoring";
-import Management from "./components/Management";
-import Testimonials from "./components/Testimonials";
-import Geography from "./components/Geography";
-import ContactForm from "./components/ContactForm";
-import Footer from "./components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import Technology from "@/components/Technology";
+import Monitoring from "@/components/Monitoring";
+import Management from "@/components/Management";
+import Testimonials from "@/components/Testimonials";
+import Geography from "@/components/Geography";
+import ContactForm from "@/components/ContactForm";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
